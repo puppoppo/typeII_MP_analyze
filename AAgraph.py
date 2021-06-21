b4ac65c064d5cf66820514c35680bcb9bd503149
RyoheiW <ryohei.a.watanabe@gmail.com>
2021-06-21 18:10:04 +0900

    no message

--- AAgraph.py
+++ AAgraph.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 f=open('aaindex1.dat', 'r')
 er=open('er.fas','r')
@@ -7,6 +8,7 @@ pm=open('pm.fas','r')
 fragI=0
 raw=''
 ID=''
+ID_array=[]
 AAall=[[]]
 
 while True:
@@ -15,16 +17,24 @@ while True:
     if data.startswith('I'):
         fragI = 1
     elif data.startswith('H'):
-        ID=data
+        ID=data[2:]
     elif fragI==1 and not data.startswith('//'):
         raw+=data
     elif data.startswith('//'):
         para = raw.split()
         AAall.append(para)
+        ID_array.append(ID)
         raw=''
         fragI=0
     elif data == '':
         break
+
+AAall.remove([])
+
+columns=['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
+
+dataframe1=pd.DataFrame(AAall,index=ID_array,columns=columns)
+dataframe1
 
 i=0
 j=0
