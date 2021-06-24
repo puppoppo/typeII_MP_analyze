cea34f76a7c2e7b56a89e80ddcea9649a1f83881
RyoheiW <ryohei.a.watanabe@gmail.com>
2021-06-24 18:34:18 +0900

    no message

--- AAgraph.py
+++ AAgraph.py
@@ -54,17 +54,17 @@ columns=[-15,-14,-13,-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,
 df_er=pd.DataFrame(fas,columns=columns)
 
 ##############################################################################################
-def AAindex_get(k,df_index):
-    l=0
-    value=[]
-    while l<len(df_index.columns):
-        if df_index.iat[k,l]=="NA":
-            value.append(0)
-        else:
-            value.append(float(df_index.iat[k,l]))
-        l=l+1
-
-        return value
+# def AAindex_get(k,df_index):
+#     l=0
+#     value=[]
+#     while l<len(df_index.columns):
+#         if df_index.iat[k,l]=="NA":
+#             value.append(0)
+#         else:
+#             value.append(float(df_index.iat[k,l]))
+#         l=l+1
+#
+#         return value
 
 
 ##############################################################################################
