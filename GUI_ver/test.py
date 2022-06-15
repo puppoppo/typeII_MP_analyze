349781c15d8bbe422d54c784354cea828d21f885
Ryohei.W <78584729+puppoppo@users.noreply.github.com>
2022-06-15 23:44:19 +0900

    Add files via upload

--- GUI_ver/test.py
+++ GUI_ver/test.py
@@ -1,20 +1,28 @@
 import sys
 import tkinter
 import os
+from tkinter import filedialog
+
+def sanshofn(file_dir):
+    typ = [('','*')]
+    fle = filedialog.askopenfilename(filetypes = typ, initialdir = file_dir)
+
 
 root = tkinter.Tk()
 root.title(u"Software Title")
 root.geometry("600x400")
 
-files_dir=os.getcwd()
+file_dir=os.getcwd()
 
 EditBox = tkinter.Entry(width=75)
-EditBox.insert(tkinter.END,files_dir)
+EditBox.insert(tkinter.END,file_dir)
 EditBox.pack()
 EditBox.place(x=15, y=15)
 
-sansho=tkinter.Button(text=u'参照')
+sansho=tkinter.Button(text=u'参照',command=sanshofn(file_dir))
 sansho.pack()
 sansho.place(x=480, y=10)
+
+
 
 root.mainloop()
