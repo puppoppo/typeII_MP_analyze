edce674a66404f9af83665a5e4c186e73904be07
RyoheiW <ryohei.a.watanabe@gmail.com>
2022-12-16 17:17:51 +0900

    no message

--- NEW_alignment/Align_NMO_pro.pl
+++ NEW_alignment/Align_NMO_pro.pl
@@ -1,6 +1,6 @@
 
-open(fasta, "FT269_fusion.fas");
-open(WRITE,">FT269_pro.csv");
+open(fasta, "npm_98.fas");
+open(WRITE,">FT269_pro2.csv");
 
 $, = ",";
 $\ = "\n";
@@ -14,10 +14,11 @@ while(<fasta>){
 	if($_ =~ /^>/){
 		$temp = substr($_,0,100);
 	}
-	else{
+	elsif($_ =~ /^X/){
 		$SEQUENCE=substr($_,0,10000);
 		@sq=split(//,$SEQUENCE);
 		@sqn=();
+		@sqA=();
 
 		for($i=0;$i<@sq;$i++){
 			if($sq[$i] =~ /A/){
@@ -85,6 +86,72 @@ while(<fasta>){
 			}
 		}
 
+		for($i=0;$i<@sq;$i++){
+			if($sq[$i] =~ /A/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /C/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /D/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /E/){
+				$sqA[$i]=1.27;
+			}
+			elsif($sq[$i] =~ /F/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /G/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /H/){
+				$sqA[$i]=1.45;
+			}
+			elsif($sq[$i] =~ /I/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /K/){
+				$sqA[$i]=3.67;
+			}
+			elsif($sq[$i] =~ /L/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /M/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /N/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /P/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /Q/){
+				$sqA[$i]=1.25;
+			}
+			elsif($sq[$i] =~ /R/){
+				$sqA[$i]=2.45;
+			}
+			elsif($sq[$i] =~ /S/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /T/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /V/){
+				$sqA[$i]=0;
+			}
+			elsif($sq[$i] =~ /W/){
+				$sqA[$i]=6.93;
+			}
+			elsif($sq[$i] =~ /Y/){
+				$sqA[$i]=5.06;
+			}
+			else{
+				printf "error" . $temp . $i. "\n";
+			}
+		}
+
 		printf WRITE $temp."\n";
 
 		@hydra=();
@@ -107,7 +174,7 @@ while(<fasta>){
 			$start=0;
 		}
 
-		@hy_info=((["hy_def","N","O","Nend"]));
+		@hy_info=((["hy_def","N","O","Nend","AmphiScore"]));
 
 		for($N=2;$N<=5;$N++){
 			for($O=2;$O<=5;$O++){
@@ -120,7 +187,7 @@ while(<fasta>){
 				$Ostart=0;
 				$defmax=$start;
 
-				for($i=$start;$i<=$start+25;$i++){
+				for($i=$start;$i<$max;$i++){
 
 					for($j=0;$j<$N;$j++){
 						if($i+$j < 0){$hy_N[$i]+= $A;} #無いところを＄A=-0.49で置き換え
@@ -139,27 +206,26 @@ while(<fasta>){
 				}
 
 				for($i=$start;$i<=$start+25-$N-$M;$i++){
-					$hy_def[$i] = -$hy_N[$i]+$hy_O[$i+$N+$M];	#startとendで符号変化 start -N, end +N
+					$hy_def[$i] = -$hy_N[$i]+$hy_O[$i+$N+$M];	#startとendで符号変化 start:-N, end:+N
 					if($hy_def[$i]>$hy_def[$defmax]){
 						$defmax=$i;
 					}
 				}
+				$Nend=$defmax+$N-1;
+
+				$AmphiScore=0;
+				for($i=0;$i<$N;$i++){
+					$AmphiScore+=$sqA[$Nend-$i];
+				}
+
+				for($i=1;$i<$O+1;$i++){
+					$AmphiScore+=$sqA[$Nend+$i];
+				}
+				$AmphiScore=$AmphiScore/($N+$O);
 
 				$Nend=$defmax+$N-1;
-				$Ostart=$defmax+$N+$M;
 
-				# printf WRITE $Nend.",".$Ostart.",".$N.$M.$O.",".$max.",".$hy_deff[$defmax].",";
-				#
-				# for($i=$Nend-15;$i<=$Ostart+15;$i++){
-				# 	if($i<=0 || $i>=@sq){
-				# 		printf WRITE "X,";
-				# 	}
-				# 	else{
-				# 		printf WRITE $sq[$i].",";
-				# 	}
-				# }
-				# printf WRITE "\n";
-				push (@hy_info,([$hy_def[$defmax],$N,$O,$Nend]));
+				push (@hy_info,([$hy_def[$defmax],$N,$O,$Nend,$AmphiScore]));
 
 			}
 		}
@@ -168,11 +234,63 @@ while(<fasta>){
 
 		@hy_info = sort{$b->[0] <=> $a->[0]} @hy_info;
 
-		for($k=0;$k<16;$k++){
-			for($l=0;$l<4;$l++){
+		for($k=0;$k<3;$k++){
+			for($l=0;$l<5;$l++){
 				printf WRITE $hy_info[$k][$l].",";
 			}
 			printf WRITE "\n";
+		}
+
+		$certainly=0;
+
+		if($hy_info[0][3]==$hy_info[1][3]){					#TOP3が一致していたらcertainly=1
+			if($hy_info[1][3]==$hy_info[2][3]){
+				$certainly=1;
+				$Nend=$hy_info[0][3];
+			}
+		}
+
+##############################################################
+		if($certainly==0){			#TOP3が不一致ならAmphiの高いものをcertainly=2で
+			@delete=splice(@hy_info,-13);
+			@hy_info = sort{$b->[4] <=> $a->[4]} @hy_info;
+			if($hy_info[0][4]!=$hy_info[1][4]){
+				$certainly=2;
+			}else{#Amphi1位と2位が一致してたら
+				if($hy_info[1][4]!=$hy_info[2][4]){
+					if($hy_info[0][3]==$hy_info[1][3]){
+						$certainly=2;
+					}else{
+						
+					}
+				}
+			}
+		}
+
+		$Nendmin=$hy_info[0][3];
+		$Nendmax=$hy_info[0][3];
+
+		for($k=1;$k<3;$k++){
+			if($Nendmax < $hy_info[$k][3]){
+				$Nendmax=$hy_info[$k][3];
+			}
+			if($Nendmin > $hy_info[$k][3]){
+				$Nendmin=$hy_info[$k][3];
+			}
+		}
+		if($certainly!=1){
+			printf WRITE ",,,,,";
+
+			for($i=$Nendmin-15;$i<=$Nendmax+35;$i++){
+				if($i<=0 || $i>=@sq){
+					printf WRITE "X,";
+				}
+				else{
+					printf WRITE $sq[$i].",";
+				}
+			}
+			printf WRITE "\n";
+
 		}
 
 
